a90490245ac57681fddeb16207a41e445c73dcbd
Todd <40245048+tcharte@users.noreply.github.com>
2021-03-18 19:40:13 -0700

    update benchmarking
    - fixed camera bug
    - cleaned bench.py
    - deleted junk

--- bench.py
+++ bench.py
@@ -44,7 +44,7 @@ def main():
                 if len(splits) < min_splits:
                     return
                     
-                print(model,weights,img_size,fps)
+                print('Detection Model:', model, 'Image Size:', img_size, 'Camera FPS:', fps)
                 frame_queue = multiprocessing.Queue(maxsize=100000)  # Queue for the instances to get frames from the class. Max ~800MB
                 sorted_detections_queue = multiprocessing.Queue(maxsize=100000)
                 count_queue = multiprocessing.Queue(maxsize=1)
@@ -68,18 +68,21 @@ def main():
 
                         total_cans, current_frame_id = count_queue.get(block=False)
                         print('  frame', current_frame_id, '/', n_loops*complete_loop, '\t\tCounted Cans:', total_cans, end='\r')
-                        if current_frame_id >= n_loops*complete_loop: #ToDo: check last 2 frames
+                        if current_frame_id >= n_loops*complete_loop: # If n_loops are completed, calculate results and stop
                             toc = time.time()
                             total_time = toc-tic
                             calculated_fps = current_frame_id/total_time
-                            real = loop_camera.get_count()
+                            if not loop_camera.real_count_queue.empty():
+                                real = loop_camera.get_count()
+                            else:
+                                real = round((current_frame_id / complete_loop) * 880)
                             error = (total_cans - real) / real * 100
-                            print('<<-------------COMPLETE------------->>')
+                            print('<<------------------COMPLETE------------------>>')
                             print('Model:', model, '-', img_size, '-', fps)
                             print('Time:', total_time)
                             print('Calculated fps:', calculated_fps)
                             print('Error', error)
-                            print('<<---------------------------------->>')
+                            print('<<-------------------------------------------->>')
                             json_data = {
                                 'Model': model,
                                 #'Weights File': weights,
@@ -102,7 +105,6 @@ def main():
 
 
         del video
-        print('video deleted')
     
     with open('benchmark.json', 'w') as outfile:
         json.dump(models, outfile)
--- cameras.py
+++ cameras.py
@@ -21,6 +21,29 @@ def frame_generator(video, frame_queue, count_queue, fps):
         total_count += 880
 
         if count_queue.full():
+            print('Count queue is full!def frame_generator(video, frame_queue, count_queue, fps):
+    import time
+    """
+    Target function for running the camera acquisition as a process. This makes more sense as the dedicated process for
+    the camera ensures it is not slowed down by the rest of the program
+    """
+    print('Pausing camera so all processes have time to start')
+    time.sleep(10)
+    print('Loop camera started!')
+    total_count = 0
+    frame_id = 0
+    while True:
+        for frame in video:
+            if frame_queue.full():
+                print('Frame queue is full! Consumers are too slow. Can count is no longer accurate! Skipping frame...')
+            else:
+                frame_queue.put((frame, frame_id))
+                frame_id += 1
+                time.sleep(1/fps)
+
+        total_count += 880
+
+        if count_queue.full():
             print('Count queue is full! Consumers are too slow. The actual can count is no longer accurate...')
         else:
             count_queue.put(total_count)
@@ -39,7 +62,34 @@ class LoopCamera:
         if process_trigger:  # Introduced a flag that only the main process can trigger so infinite process do not spawn
             if self.generator is None:
                 import multiprocessing
-                self.generator = multiprocessing.Process(target=frame_generator, args=(video, frame_queue, LoopCamera.real_count_queue, fps))
+                self.generator = multiprocessing.Process(target=frame_generator, args=(video, frame_queue, self.real_count_queue, fps))
+                self.generator.start()
+
+    def get_count(self):
+        return self.real_count_queue.get()
+
+    def terminate(self):
+        self.generator.terminate()
+        return
+ Consumers are too slow. The actual can count is no longer accurate...')
+        else:
+            count_queue.put(total_count)
+
+
+class LoopCamera:
+    import multiprocessing
+    """
+    Simple looping camera that uses the footage of a known amount of cans. The camera will track the can count internally
+    to measure against the calculated can count
+    """
+    real_count_queue = multiprocessing.Queue(maxsize=100000)  # Max will be quite small since this holds integers
+    generator = None
+
+    def __init__(self, video, frame_queue, fps, process_trigger=False):
+        if process_trigger:  # Introduced a flag that only the main process can trigger so infinite process do not spawn
+            if self.generator is None:
+                import multiprocessing
+                self.generator = multiprocessing.Process(target=frame_generator, args=(video, frame_queue, self.real_count_queue, fps))
                 self.generator.start()
 
     def get_count(self):
