3ba1dd9006e948c3bc57b7446d3a0ec5684bde27
Todd <40245048+tcharte@users.noreply.github.com>
2021-05-07 12:02:08 -0700

    Clean up!
    Deleted a lot of stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# yolov5-Can-Counter
+## Machine Vision for Counting Aluminum Cans Falling Off of a Conveyor.
 
--- python_test.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-import queue
-
-
-quwu = multiprocessing.Queue(maxsize=100000)
-try:
-    yeet = quwu.get(block=False)
-except queue.Empty:
-    print('uh oh empty')
